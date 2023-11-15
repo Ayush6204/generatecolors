0bc2972d88ee72175f31f1e4dd3af52f7c02c332
Ayush6204 <ayushkumarrocc1201@gmail.com>
2023-11-15 12:22:08 +0530

    adding movie

---
+++ movie.js
@@ -0,0 +1,7 @@
+let favouritemovie="spiderman";
+let guess=prompt("enter movie name");
+while(guess!=favouritemovie && guess!="Quit"){
+console.log("try agin");
+guess=prompt("enter movie name");
+}
+console.log("bhaag bsdk");
